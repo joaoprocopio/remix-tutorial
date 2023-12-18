7561842ada97e294bfbff85bb1981086f2220a0c
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-12-18 02:11:48 -0300

    this is genius...

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,7 @@
 import type {
   ActionFunction,
   LinksFunction,
+  LoaderFunctionArgs,
   MetaFunction,
 } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
@@ -34,8 +35,10 @@ export const action: ActionFunction = async () => {
   return redirect(`/contacts/${contact.id}/edit`);
 };
 
-export const loader = async () => {
-  const contacts = await getContacts();
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
 
   return json({ contacts });
 };
